b3049e8ce4405be96e3d890ddd212e8607fe2ba0
Brina <bz.brina@gmail.com>
2025-03-12 14:17:21 +0100

    Add files via upload

---
+++ Helpers/SessionsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using OpremiSe.Models;
+
+namespace OpremiSe.Helpers
+
+{
+    public static class SessionsExtensions
+    {
+        public static void SetCart(this ISession session, string key, ShoppingCart cart)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(cart));
+        }
+
+        public static ShoppingCart GetCart(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? new ShoppingCart() : JsonConvert.DeserializeObject<ShoppingCart>(value);
+        }
+    }
+}
